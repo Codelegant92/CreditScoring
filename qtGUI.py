0754f6e0e4976ae7e51bc9c886ab8713314f925f
CHEN Yangbin <robinchan1992@gmail.com>
2015-09-21 18:55:21 +0800

    develop a GUI application for credit scoring app using PyQt4

--- qtGUI.py
+++ qtGUI.py
@@ -3,9 +3,16 @@ __author__ = 'robin'
 import sys
 from PyQt4 import QtGui
 
-app = QtGui.QApplication(sys.argv)
-w = QtGui.QWidget()
-w.resize(250, 150)
-w.move(300, 300)
-w.setWindowTitle('TEST')
-sys.exit(app.exec_())
+def main():
+    app = QtGui.QApplication(sys.argv)
+
+    w = QtGui.QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('TEST')
+    w.show()
+
+    sys.exit(app.exec_())
+
+if(__name__ == "__main__"):
+    main()
